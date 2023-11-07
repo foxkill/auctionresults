ab4b2354de17401709575f99389f6c68ad3597b4
martinpfh <martin@pfh.de>
2023-11-07 14:51:41 +0100

    Add url const

--- Announced.py
+++ Announced.py
@@ -3,11 +3,12 @@
 #
 
 class Announced:
+    URL = f"https://www.treasurydirect.gov/TA_WS/securities/announced?format=html&type={type}"
     """docstring for Announced."""
     def __init__(self, arg):
         self.arg = arg
         return
 
-    def get(self):
-        # https://www.treasurydirect.gov/TA_WS/securities/announced?format=html&type=FRN
+    def get(self, type):
+        url = self.URL
         return
